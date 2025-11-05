cc49b8af2f200a4402462289e6d96a5d9ff5c139
Anwesha <anweshadas909@gmail.com>
2025-11-05 10:47:20 +0530

    refac:error fixed

--- flitnex-frontend/src/environments/environment.ts
+++ flitnex-frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5000/api',
+  // apiBaseUrl: 'http://localhost:5000/api',
+  apiBaseUrl: 'https://shipthis-fletnix.onrender.com', //deployed backend URL
 };
 

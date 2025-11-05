54d63771ae677ac6a1356aaaaff2a223f955fb83
Anwesha <anweshadas909@gmail.com>
2025-11-05 11:45:00 +0530

    refac: prod uri

--- flitnex-frontend/src/environments/environment.prod.ts
+++ flitnex-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://shipthis-fletnix.onrender.com', // Replace with actual production API URL
+  apiBaseUrl: 'https://shipthis-fletnix.onrender.com/api', // Replace with actual production API URL
 };
 
